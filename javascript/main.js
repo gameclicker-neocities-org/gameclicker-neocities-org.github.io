b2ef2b7b8c7be69c8cf34b476786afd47bd70f3e
hackgames13 <159486747+hackgames13@users.noreply.github.com>
2024-07-01 15:27:17 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -4,11 +4,11 @@ const button_click = document.getElementById('button_click');
 
 function text_click() {
     text.textContent = "Coins: " + coins;
+    document.title = "Coins: " + coins + " - game clicker";
 }
 
 text_click();
 button_click.addEventListener('click', function() {
   coins += 1;
   text_click();
-  document.title = ``;
 });
